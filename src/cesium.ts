35b7587c87871b5182bbb45998b28e34a252b695
Homan <homanwang@hotmail.com>
2026-05-24 11:49:46 +0800

    feat: Update cookies with camera position and zoom level on movement

--- src/cesium.ts
+++ src/cesium.ts
@@ -6,6 +6,7 @@ import "cesium/Build/Cesium/Widgets/widgets.css";
 import * as Cesium from "cesium";
 import { Viewer, Cartesian3 } from "cesium";
 import { getPosition } from "./map";
+import { setCookie } from "./utils";
 
 // Tell Cesium where to find its assets (Images, Workers, etc.)
 // Since we use the CDN for the main library, we should also use it for assets.
@@ -16,7 +17,7 @@ window.CESIUM_BASE_URL = "https://cdn.jsdelivr.net/npm/cesium@1.141.0/Build/Cesi
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkZGViN2YzNC1hYzgyLTQ2ZTQtYTEyMS0wZGYwOTY2ZWJiMzEiLCJpZCI6NDM1NTgyLCJzdWIiOiJob21hbncxMDQiLCJpc3MiOiJodHRwczovL2FwaS5jZXNpdW0uY29tIiwiYXVkIjoiSUlUQyBOZXh0IiwiaWF0IjoxNzc5NTY3OTg4fQ.YBXp3trSarnjwb9R2G5sU57DC0VbI0iCJrZv7TyuZFk";
 
 /**
- * Load the Cesium library and initialize a Viewer
+ * Load the Cesium library and initialize a Viewer.
  */
 export default function loadCesium(): Viewer {
   // Create container div where the viewer will be placed
@@ -62,6 +63,25 @@ export default function loadCesium(): Viewer {
       destination: Cartesian3.fromDegrees(pos.center[1], pos.center[0], height),
     });
   }
+
+  // Update cookies when the camera moves
+  viewer.camera.moveEnd.addEventListener(() => {
+    const camera = viewer.camera;
+    const cartographic = camera.positionCartographic;
+    const lat = Cesium.Math.toDegrees(cartographic.latitude);
+    const lng = Cesium.Math.toDegrees(cartographic.longitude);
+    const height = cartographic.height;
+
+    // Convert height back to zoom level
+    // height = 40000000 / Math.pow(2, zoom)
+    // Math.pow(2, zoom) = 40000000 / height
+    // zoom = log2(40000000 / height)
+    const zoom = Math.round(Math.log2(40000000 / height));
+
+    setCookie("ingress.intelmap.lat", lat.toFixed(6));
+    setCookie("ingress.intelmap.lng", lng.toFixed(6));
+    setCookie("ingress.intelmap.zoom", zoom.toString());
+  });
 
   return viewer;
 }
