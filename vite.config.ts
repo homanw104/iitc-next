67a984ae958d8ae107c86a8407c709caf996b23f
Homan <homanwang@hotmail.com>
2026-05-26 22:42:34 +0800

    fix: Cesium css url at build time

--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,18 @@ export default defineConfig({
       entry: "src/app.ts",
       userscript: {
         name: "iitc-next",
+        author: "Homan",
+        description: "IITC Next",
         namespace: "npm/vite-plugin-monkey",
         match: ["https://intel.ingress.com/*"],
-        "run-at": "document-start",
+        "run-at": "document-end",
       },
       build: {
         externalGlobals: {
           cesium: cdn.jsdelivr("Cesium", "Build/Cesium/Cesium.js"),
         },
         externalResource: {
-          "cesium/Build/Cesium/Widgets/widgets.css": cdn.jsdelivr(
-            "Build/Cesium/Widgets/widgets.css"
-          ),
+          "cesium/Build/Cesium/Widgets/widgets.css": "https://cdn.jsdelivr.net/npm/cesium@1.141.0/Build/Cesium/Widgets/widgets.css",
         },
       },
     }),
