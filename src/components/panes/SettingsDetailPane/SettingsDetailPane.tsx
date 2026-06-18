212a0f0cf16712c093cace23a2ddd4d6c0880c51
Homan <homanwang@hotmail.com>
2026-06-18 11:18:38 +0800

    feat: Add reload button for Google 3D Tiles settings

--- src/components/panes/SettingsDetailPane/SettingsDetailPane.tsx
+++ src/components/panes/SettingsDetailPane/SettingsDetailPane.tsx
@@ -1,5 +1,6 @@
 import { h } from "../../../utils/dom.ts";
 import { settingsManager } from "../../../managers/settingsManager.ts";
+import { safeWindow } from "../../../utils/window.ts";
 import BackButton from "../../atoms/BackButton/BackButton.tsx";
 import CloseButton from "../../atoms/CloseButton/CloseButton.tsx";
 import RightArrowIcon from "./RightArrowIcon.tsx";
@@ -17,6 +18,14 @@ const SettingsDetailPane = ({
   onShowLoggingDetail: () => void,
   onShowPluginDetail: () => void,
 }) => {
+  const initialUseGoogle3dTiles = settingsManager.getUseGoogle3dTiles();
+  let reloadButton: HTMLAnchorElement | null = null;
+
+  const updateReloadButtonVisibility = (useGoogle3dTiles: boolean) => {
+    if (!reloadButton) return;
+    reloadButton.style.display = useGoogle3dTiles === initialUseGoogle3dTiles ? "none" : "inline";
+  };
+
   return (
     <div
       style={{
@@ -61,19 +70,37 @@ const SettingsDetailPane = ({
           <div style={{ fontWeight: "bold" }}>Use Google 3D Tiles</div>
           <div style={{ fontSize: "12px", color: "#aaa" }}>Restart is required to take effect.</div>
         </div>
-        <input
-          type="checkbox"
-          checked={settingsManager.getUseGoogle3dTiles()}
-          onClick={(e: Event) => {
-            const target = e.target as HTMLInputElement;
-            settingsManager.setUseGoogle3dTiles(target.checked);
-          }}
-          style={{
-            width: "20px",
-            height: "20px",
-            cursor: "pointer",
-          }}
-        />
+        <div style={{ display: "flex", alignItems: "center", gap: "12px" }}>
+          <a
+            id="reload-google-3d-tiles"
+            ref={(el: HTMLAnchorElement) => {
+              reloadButton = el;
+              updateReloadButtonVisibility(settingsManager.getUseGoogle3dTiles());
+            }}
+            style={{
+              color: "#6088ff",
+              cursor: "pointer",
+              display: "none",
+            }}
+            onClick={() => safeWindow?.location.reload()}
+          >
+            Reload
+          </a>
+          <input
+            type="checkbox"
+            checked={initialUseGoogle3dTiles}
+            onClick={(e: Event) => {
+              const target = e.target as HTMLInputElement;
+              settingsManager.setUseGoogle3dTiles(target.checked);
+              updateReloadButtonVisibility(target.checked);
+            }}
+            style={{
+              width: "20px",
+              height: "20px",
+              cursor: "pointer",
+            }}
+          />
+        </div>
       </div>
       <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
         <div
